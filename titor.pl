e4f9d02cbf0c66705f96ee12dce409e9f1044770
Chris <chris@starforge.co.uk>
2016-06-07 00:08:17 +0100

    Start work on making a wording main script.

--- titor.pl
+++ titor.pl
@@ -2,4 +2,33 @@
 
 use strict;
 use v5.14;
-use lib qw(modules);
+
+use FindBin;             # Work out where we are
+my $path;
+BEGIN {
+    $ENV{"PATH"} = "/bin:/usr/bin"; # safe path.
+
+    # $FindBin::Bin is tainted by default, so we may need to fix that
+    # NOTE: This may be a potential security risk, but the chances
+    # are honestly pretty low...
+    if ($FindBin::Bin =~ /(.*)/) {
+        $path = $1;
+    }
+}
+use lib "$path/modules"; # Add the script path for module loading
+
+use Titor qw(path_join);
+use Titor::Backup;
+use Titor::Database;
+use Titor::ConfigMicro;
+use Log::Log4perl;
+use PID::File;
+
+use constant PIDFILENAME => "/var/run/titor";
+
+Log::Log4perl -> init(path_join($path, "config", "logging.cnf"));
+my $logger = Log::Log4perl -> get_logger();
+
+my $pid_file = PID::File -> new(file => PIDFILENAME);
+$logger -> logdie("Titor is already running. Only one Titor may exist at any time.")
+    if($pid_file -> running);
