14b6531c03be8c3281a77986e0c670c27f8e49d0
Chris <chris@starforge.co.uk>
2016-06-09 09:45:50 +0100

    Progress.

--- titor.pl
+++ titor.pl
@@ -26,9 +26,47 @@ use PID::File;
 
 use constant PIDFILENAME => "/var/run/titor";
 
+
+sub load_config {
+    my $config = shift;
+    my $logger = shift;
+
+    # Ensure the config file is valid, and exists
+    my ($configfile) = $config =~ /^(\w+)$/;
+    $logger -> logdie("The specified config file name is not valid, or does not exist")
+        if(!$configfile || !-f path_join($path, "config", $configfile.".cfg"));
+
+    # Bomb if the config file is not at most 600
+    my $mode = (stat(path_join($path, "config", $configfile.".cfg")))[2];
+    $logger -> logdir("$configfile.cfg must have at most mode 600.\nFix the permissions on $configfile.cfg and try again.")
+        if($mode & 07177);
+
+    # Load the configuration
+    my $config = ConfigMicro -> new(path_join($path, "config", $configfile.".cfg"))
+        or $logger -> logdie("Unable to load configuration. Error was: $Titor::errstr")
+
+    # Store the config name for later
+    $config -> {"configname"} = $configfile;
+
+    return $config;
+}
+
+
 Log::Log4perl -> init(path_join($path, "config", "logging.cnf"));
 my $logger = Log::Log4perl -> get_logger();
 
 my $pid_file = PID::File -> new(file => PIDFILENAME);
 $logger -> logdie("Titor is already running. Only one Titor may exist at any time.")
-    if($pid_file -> running);
+    if($pid_file -> running());
+
+if($pid_file -> create()) {
+    # Note no $pid_file -> guard here: if the process dies, we don't want it to
+    # clean up nicely as something Awful And Hideous may have happened that could
+    # be made worse by additional runs; require user intervention!
+
+
+
+    $pid_file -> remove();
+} else {
+    $logger -> error("Unable to create PID file, aborting.");
+}
